76761832dce3ada1e03bdb1760b64bbaf7e15e8f
valak74200 <clmentlv@outlook.fr>
2025-01-04 21:22:12 +0100

    Amélioration de la fonction execute_command
    - Ajout du paramètre program_name pour une meilleure gestion des erreurs
    - Utilisation de perror pour afficher les erreurs système
    - Amélioration du message d'erreur en cas de commande non trouvée
    - Utilisation de waitpid au lieu de wait pour une meilleure flexibilité
    - Ajout d'un code de sortie spécifique (127) pour les commandes non trouvées

--- execute.c
+++ execute.c
@@ -3,35 +3,37 @@
 /**
  * execute_command - Executes the given command
  * @command: The command to execute
+ * @program_name: Name of the shell program (argv[0])
  *
  * Description: This function creates a child process using fork() and
  * executes the given command using execve(). The parent process waits
- * for the child to complete using waitpid().
+ * for the child to complete using waitpid(). It handles errors and
+ * provides appropriate error messages.
  */
-void execute_command(char *command)
+void execute_command(char *command, char *program_name)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
+	pid = fork();
 
-    if (pid == -1)
-    {
-        perror("Error:");
-        return;
-    }
-    else if (pid == 0)
-    {
-        char *args[] = {command, NULL};
+	if (pid == -1)
+	{
+		perror(program_name);
+		return;
+	}
+	else if (pid == 0)
+	{
+		char *args[] = {command, NULL};
 
-        if (execve(command, args, environ) == -1)
-        {
-            printf("%s: No such file or directory\n", command);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
+		if (execve(command, args, environ) == -1)
+		{
+			fprintf(stderr, "%s: 1: %s: not found\n", program_name, command);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
