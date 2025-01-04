01fefaf41a45bcaf2bec47cd40cc8acfe77b937c
valak74200 <clmentlv@outlook.fr>
2025-01-04 18:41:19 +0100

    Définir le header principal du shell
    Ajoute le fichier d'en-tête shell.h qui définit la structure de base du shell. Inclut :
    - Inclusion des bibliothèques standard nécessaires
    - Déclaration de la variable externe environ
    - Prototypes des fonctions principales du shell
    - Protection contre les inclusions multiples avec les macros de préprocesseur

--- shell.h
+++ shell.h
@@ -5,23 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <signal.h>
-
-#define MAX_ARGS 64
-#define MAX_PIPES 10
 
 extern char **environ;
 
 void sigint_handler(int sig);
 int execute_input(char *input);
-char **parse_command(char *command);
-int execute_builtin(char **args);
-int execute_external(char **args);
-char *find_executable(char *cmd);
-void handle_redirection(char **args);
-int execute_pipeline(char *input);
+char *parse_command(char *input);
+int execute_command(char *command);
 
-#endif
+#endif /* SHELL_H */
