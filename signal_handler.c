2aee2f075b6ae7eb5265cf5872981203b2c276fb
valak74200 <clmentlv@outlook.fr>
2025-01-04 18:48:49 +0100

    Implémenter le gestionnaire de signal SIGINT
    Ajoute la fonction sigint_handler pour gérer le signal SIGINT (Ctrl+C). Fonctionnalités :
    - Ignore le paramètre du signal pour éviter les avertissements du compilateur
    - Affiche un nouveau prompt sur une nouvelle ligne
    - Vide le buffer de sortie pour assurer l'affichage immédiat du prompt

--- signal_handler.c
+++ signal_handler.c
@@ -3,6 +3,9 @@
 /**
  * sigint_handler - Handle SIGINT signal (Ctrl+C)
  * @sig: The signal number
+ *
+ * Description: This function is called when a SIGINT signal is received.
+ * It prints a new prompt instead of terminating the shell.
  */
 void sigint_handler(int sig)
 {
