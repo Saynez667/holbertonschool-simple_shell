a0f1d068446e130986fd280fa5d495f1169ce9df
valak74200 <clmentlv@outlook.fr>
2025-01-04 18:43:54 +0100

    Implémenter la fonction parse_command
    Ajoute la fonction parse_command pour extraire la commande de l'entrée utilisateur. Caractéristiques :
    - Utilisation de strtok pour séparer la commande des arguments
    - Traitement des espaces, tabulations et autres caractères de séparation
    - Retourne le premier mot de l'entrée comme commande
    - Retourne NULL si aucune commande n'est trouvée

--- parse_command.c
+++ parse_command.c
@@ -1,43 +1,15 @@
 #include "shell.h"
 
 /**
- * parse_command - Parse the command string into arguments
- * @command: The command string to parse
- * 
- * Return: An array of strings containing the command arguments
+ * parse_command - Parse the input to extract the command
+ * @input: The input string to parse
+ *
+ * Description: This function takes the input string and extracts
+ * the first word as the command, ignoring any additional arguments.
+ *
+ * Return: The parsed command, or NULL if no command found
  */
-char **parse_command(char *command)
+char *parse_command(char *input)
 {
-    int bufsize = MAX_ARGS;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
-
-    if (!tokens)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(command, " \t\r\n\a");
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize)
-        {
-            bufsize += MAX_ARGS;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                perror("realloc");
-                exit(EXIT_FAILURE);            
-            }
-        }
-
-        token = strtok(NULL, " \t\r\n\a");
-    }
-    tokens[position] = NULL;
-    return (tokens);
+	return (strtok(input, " \t\r\n\a"));
 }
