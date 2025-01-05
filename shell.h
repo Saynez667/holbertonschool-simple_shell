1dadc9a2d7fa3b9f0083291d8d9d662699dbd58f
valak74200 <clmentlv@outlook.fr>
2025-01-05 04:20:32 +0100

    Add search path file

--- shell.h
+++ shell.h
@@ -1,12 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/*LIBRARIES*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 /* GLOBAL VAR*/
 extern char **environ;
@@ -17,5 +19,6 @@ int _strcmp(const char *s1, const char *s2);
 void _env(void);
 int empty_line(const char *buff);
 char **tokenize(char *buff);
+char *search_path(char *command);
 
 #endif
