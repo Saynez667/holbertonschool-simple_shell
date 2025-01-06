18350af0916016e6555b474fed9bdfbf1ec33fd9
valak74200 <clmentlv@outlook.fr>
2025-01-06 19:52:10 +0100

    Revert the file

--- exec.c
+++ exec.c
@@ -1,6 +1,37 @@
 #include "shell.h"
 
 /**
+ * execute_builtin - Execute built-in commands
+ * @args: Command arguments
+ * Return: 1 if builtin executed, 0 otherwise
+ */
+int execute_builtin(char **args)
+{
+	if (!args || !args[0])
+		return (0);
+
+	if (_strcmp(args[0], "exit") == 0)
+	{
+		free_args(args);
+		exit(_atoi(args[1] ? args[1] : "0"));
+	}
+
+	if (_strcmp(args[0], "env") == 0)
+	{
+		print_env();
+		return (1);
+	}
+
+	if (_strcmp(args[0], "cd") == 0)
+	{
+		change_directory(args);
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
  * execute_command - Execute a command with arguments
  * @args: Command and arguments
  * Return: Exit status
@@ -46,35 +77,4 @@ int execute_command(char **args)
 	waitpid(pid, &status, 0);
 	free(cmd_path);
 	return (WEXITSTATUS(status));
-}
-
-/**
- * execute_builtin - Execute built-in commands
- * @args: Command arguments
- * Return: 1 if builtin executed, 0 otherwise
- */
-int execute_builtin(char **args)
-{
-	if (!args || !args[0])
-		return (0);
-
-	if (_strcmp(args[0], "exit") == 0)
-	{
-		free_args(args);
-		exit(_atoi(args[1] ? args[1] : "0"));
-	}
-
-	if (_strcmp(args[0], "env") == 0)
-	{
-		print_env();
-		return (1);
-	}
-
-	if (_strcmp(args[0], "cd") == 0)
-	{
-		change_directory(args);
-		return (1);
-	}
-
-	return (0);
 }
