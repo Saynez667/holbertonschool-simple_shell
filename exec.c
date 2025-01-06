19881e1064ae59c6ac2fd28dc82cecc08431233c
valak74200 <clmentlv@outlook.fr>
2025-01-06 17:55:23 +0100

    New files for requirements

--- exec.c
+++ exec.c
@@ -10,35 +10,41 @@
  */
 void handle_redirection(char **args)
 {
-	int i;
-	int fd;
+    int i;
+    int fd;
 
-	for (i = 0; args[i] != NULL; i++)
-	{
-		if (strcmp(args[i], ">") == 0)
-		{
-			if (args[i + 1] == NULL)
-			{
-				fprintf(stderr, "No file specified for redirection\n");
-				return;
-			}
-			args[i] = NULL;
-			fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd == -1)
-			{
-				perror("open");
-				return;
-			}
-			if (dup2(fd, STDOUT_FILENO) == -1)
-			{
-				perror("dup2");
-				close(fd);
-				return;
-			}
-			close(fd);
-			break;
-		}
-	}
+    for (i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], ">") == 0)
+        {
+            if (args[i + 1] == NULL)
+            {
+                fprintf(stderr, "No file specified for redirection\n");
+                return;
+            }
+            args[i] = NULL;
+            fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1)
+            {
+                perror("open");
+                return;
+            }
+            if (close(STDOUT_FILENO) == -1)
+            {
+                perror("close");
+                close(fd);
+                return;
+            }
+            if (open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644) != STDOUT_FILENO)
+            {
+                fprintf(stderr, "Failed to redirect output\n");
+                close(fd);
+                return;
+            }
+            close(fd);
+            break;
+        }
+    }
 }
 
 /**
