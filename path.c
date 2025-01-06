e06b8110bc82cb08953180399c9e3ef6949bf75a
valak74200 <clmentlv@outlook.fr>
2025-01-06 18:57:40 +0100

    New path.c file

--- path.c
+++ path.c
@@ -13,34 +13,39 @@ extern char **environ;
  */
 char *find_command_in_path(const char *command)
 {
-	char *path = _getenv("PATH", environ);
-	char *path_copy, *dir, *full_path;
+	char *path, *path_copy, *dir, *full_path;
 	struct stat st;
 
-	if (!command || (command[0] == '/' || command[0] == '.'))
+	if (!command)
+		return (NULL);
+	if (command[0] == '/' || command[0] == '.')
 	{
 		if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
 			return (_strdup(command));
 		return (NULL);
 	}
-
-	if (!path || !(path_copy = _strdup(path)))
+	path = _getenv("PATH", environ);
+	if (!path)
 		return (NULL);
-
+	path_copy = _strdup(path);
+	if (!path_copy)
+		return (NULL);
 	dir = strtok(path_copy, ":");
 	while (dir)
 	{
 		full_path = malloc(_strlen(dir) + _strlen(command) + 2);
-		if (full_path)
+		if (!full_path)
 		{
-			sprintf(full_path, "%s/%s", dir, command);
-			if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
-			{
-				free(path_copy);
-				return (full_path);
-			}
-			free(full_path);
+			free(path_copy);
+			return (NULL);
 		}
+		sprintf(full_path, "%s/%s", dir, command);
+		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
 		dir = strtok(NULL, ":");
 	}
 	free(path_copy);
