bbebd9d27d35ccab60c26f1f8d935713eb7e9148
valak74200 <clmentlv@outlook.fr>
2025-01-07 23:33:54 +0100

    Refactor prompt function: update file permissions and enhance prompt printing logic with fflush for better output handling.

--- prompt.c
+++ prompt.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
-  * print_prompt - Print the prompt to the user
-  */
+	* print_prompt - Print the prompt to the user
+	*/
 void print_prompt(void)
 {
-  /* isatty() vérifie si STDIN est un terminal */
+	/* isatty() vérifie si STDIN est un terminal */
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "ValakShell$ ", 12);
+		fflush(stdout);
 }
