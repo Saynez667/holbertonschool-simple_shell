9b6e6902320ec9bf8cb318caafc59ebd59d038a0
valak74200 <clmentlv@outlook.fr>
2025-01-07 15:36:55 +0100

    Refactor print_error function: update parameter names for clarity, enhance documentation, and improve error message formatting

--- print_error.c
+++ print_error.c
@@ -1,17 +1,20 @@
 #include "shell.h"
 
 /**
- * print_error - Print error message to stderr
- * @program: Program name
- * @cmd: Command that failed
- * @msg: Error message
- */
-void print_error(char *program, char *cmd, char *msg)
+* print_error - Print error message to stderr
+* @program_name: Name of the program
+* @cmd: Command that failed
+* @error_msg: Error message to display
+*
+* Description: Prints formatted error message with program name,
+* command and error message to standard error
+*/
+void print_error(char *program_name, char *cmd, char *error_msg)
 {
-		write(2, program, _strlen(program));
-		write(2, ": 1: ", 4);
-		write(2, cmd, _strlen(cmd));
-		write(2, ": ", 2);
-		write(2, msg, _strlen(msg));
-		write(2, "\n", 1);
+		write(STDERR_FILENO, program_name, _strlen(program_name));
+		write(STDERR_FILENO, ": 1: ", 5);
+		write(STDERR_FILENO, cmd, _strlen(cmd));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, error_msg, _strlen(error_msg));
+		write(STDERR_FILENO, "\n", 1);
 }
