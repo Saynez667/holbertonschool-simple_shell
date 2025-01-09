a943c1868790c5445143c13314429b8ae05432d7
valak74200 <clmentlv@outlook.fr>
2025-01-09 12:05:03 +0100

    Refactor command path handling to include environment variable support in handle_command_path and get_file_path functions.

--- shell.h
+++ shell.h
@@ -16,7 +16,7 @@
 int execute_command(char *input, char *argv[],
 		char **env, char *program_name);
 void execute_child(char *cmd_path, char **args, char **env);
-char *handle_command_path(char **args, char *program_name);
+char *handle_command_path(char **args, char *program_name, char **env);
 int get_token_count(char **args);
 
 /* Input and Tokenization Functions */
@@ -27,11 +27,11 @@ void free_tokens(char **args, int count);
 char *trim_token(char *token);
 
 /* Path Functions */
+char *get_file_path(char *command, char **env);
 char *search_path(char *command, char *path);
 char *check_current_dir(char *command);
 char *trim_spaces(char *command);
 char *concat_path(char *dir, char *command);
-char *get_file_path(char *command, char **env);
 
 /* String utility functions */
 int _strlen(char *str);
@@ -47,6 +47,7 @@ void print_error(char *program, char *cmd, char *msg);
 int handle_builtin_commands(char **args,
 		int num_args, char *input,
 		char **env);
+void handle_cd(char **args, int num_args, char **env);
 void handle_exit(char *input, int exit_status);
 int shell_exit(char **args, char *input);
 
@@ -57,11 +58,10 @@ ssize_t custom_getline(char **lineptr, size_t *n, int fd);
 void handle_sigint(int sig);
 void setup_signal_handlers(void);
 
-/* Environment variable commands */
+/* Environment variable functions */
 int handle_setenv(char **args, char *program_name);
 int handle_unsetenv(char **args, char *program_name);
 void print_env(char **env);
 char *_getenv(const char *name, char **env);
-void handle_cd(char **args, int num_args, char **env);
 
 #endif /* SHELL_H */
